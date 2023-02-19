b6aaa4fb000e9bd3db86f6affc7b3aa5d8613012
johandro7 <119898898+johandro7@users.noreply.github.com>
2023-02-19 10:27:01 -0300

    Create edades-etapas.js

---
+++ edades-etapas.js
@@ -0,0 +1,30 @@
+	var edad=15;
+
+	if (edad<=12)
+
+		{
+			console.log('usted es un niño');
+	} 
+	else
+	{
+		if ((edad>=13) && (edad<=17))
+		{
+			console.log('usted es adolecente');
+
+		}
+		else
+		{
+			if ((edad>=18) && (edad<=54))
+			{
+				console.log('usted es un adulto');
+			}
+			else
+			{
+				console.log('usted es un adulto mayor');
+			}
+
+
+		}
+
+	}
+}
